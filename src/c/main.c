23b02d5a9b694f40c34de67a63d93efc09192c50
MasatakaMiki <masataka.miki@gmail.com>
2016-09-27 17:07:54 +0900

    complete except weather forecast

--- src/c/main.c
+++ src/c/main.c
@@ -2,18 +2,18 @@
 
 static Window *s_main_window;
 static BitmapLayer *s_background_layer, *s_bt_icon_layer;
+static BitmapLayer *s_weather_icon_layer;
 static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
+static GBitmap *s_weather_icon_bitmap;
 static TextLayer *s_battery_layer;
 static TextLayer *s_time_layer;
-static TextLayer *s_date_top_layer;
-static TextLayer *s_date_btm_layer;
-static TextLayer *s_weather_layer;
+static TextLayer *s_date_top_layer, *s_date_btm_layer;
+static TextLayer *s_temperature_layer, *s_local_layer;
 static TextLayer *s_forecast_layer;
 static GFont s_battery_font;
 static GFont s_time_font;
 static GFont s_date_font;
-static GFont s_weather_font;
-static GFont s_forecast_font;
+static GFont s_temperature_font, s_local_font, s_forecast_font;
 static char s_battery_buffer[16];
 
 static void bluetooth_callback(bool connected) {
@@ -29,6 +29,13 @@ static void bluetooth_callback(bool connected) {
 static void battery_handler(BatteryChargeState charge_state) {
   snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%%", charge_state.charge_percent);
   text_layer_set_text(s_battery_layer, s_battery_buffer);
+  if(charge_state.charge_percent <= 20) {
+    text_layer_set_text_color(s_battery_layer, GColorRed);
+  } else if(charge_state.charge_percent <= 50) {
+    text_layer_set_text_color(s_battery_layer, GColorChromeYellow);
+  } else {
+    text_layer_set_text_color(s_battery_layer, GColorWhite);    
+  }
 }
 
 static void update_time() {
@@ -58,6 +65,7 @@ static void main_window_load(Window *window) {
   // Create GBitmap
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
   s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_DISCONN);
+  s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_00);
   // Create BitmapLayer to display the GBitmap
   s_background_layer = bitmap_layer_create(bounds);
   // Set the bitmap onto the layer and add to the window
@@ -67,27 +75,34 @@ static void main_window_load(Window *window) {
   s_bt_icon_layer = bitmap_layer_create(GRect(10, 12, 18, 18));
   bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
+  // weather
+  s_weather_icon_layer = bitmap_layer_create(GRect(20, 98, 50, 30));
+  bitmap_layer_set_bitmap(s_weather_icon_layer, s_weather_icon_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_weather_icon_layer));
 
   // Create the TextLayer with specific bounds
   s_battery_layer = text_layer_create(GRect(10, 10, bounds.size.w - 20, 18));
-  s_time_layer = text_layer_create(GRect(0, 26, bounds.size.w, 46));
-  s_date_top_layer = text_layer_create(GRect(18, 72, bounds.size.w - 36, 18));
-  s_date_btm_layer = text_layer_create(GRect(10, 86, bounds.size.w - 20, 18));
-  s_weather_layer = text_layer_create(GRect(0, 106, bounds.size.w, 24));
-  s_forecast_layer = text_layer_create(GRect(0, 130, bounds.size.w, 30));
+  s_time_layer = text_layer_create(GRect(0, 24, bounds.size.w, 46));
+  s_date_top_layer = text_layer_create(GRect(20, 66, bounds.size.w - 40, 18));
+  s_date_btm_layer = text_layer_create(GRect(20, 80, bounds.size.w - 40, 18));
+  s_temperature_layer = text_layer_create(GRect(60, 98, bounds.size.w - 80, 20));
+  s_local_layer = text_layer_create(GRect(60, 116, bounds.size.w - 80, 12));
+  s_forecast_layer = text_layer_create(GRect(10, 128, bounds.size.w - 20, 30));
   // background color
   text_layer_set_background_color(s_battery_layer, GColorClear);
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_background_color(s_date_top_layer, GColorClear);
   text_layer_set_background_color(s_date_btm_layer, GColorClear);
-  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_background_color(s_temperature_layer, GColorClear);
+  text_layer_set_background_color(s_local_layer, GColorClear);
   text_layer_set_background_color(s_forecast_layer, GColorClear);
   // forecolor
   text_layer_set_text_color(s_battery_layer, GColorWhite);
   text_layer_set_text_color(s_time_layer, GColorOrange);
   text_layer_set_text_color(s_date_top_layer, GColorWhite);
-  text_layer_set_text_color(s_date_btm_layer, GColorWhite);
-  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_color(s_date_btm_layer, GColorYellow);
+  text_layer_set_text_color(s_temperature_layer, GColorWhite);
+  text_layer_set_text_color(s_local_layer, GColorWhite);
   text_layer_set_text_color(s_forecast_layer, GColorWhite);
   // font
   s_battery_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_12));
@@ -97,8 +112,10 @@ static void main_window_load(Window *window) {
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_14));
   text_layer_set_font(s_date_top_layer, s_date_font);
   text_layer_set_font(s_date_btm_layer, s_date_font);
-  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_18));
-  text_layer_set_font(s_weather_layer, s_weather_font);
+  s_temperature_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_18));
+  text_layer_set_font(s_temperature_layer, s_temperature_font);
+  s_local_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_10));
+  text_layer_set_font(s_local_layer, s_local_font);
   s_forecast_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_APPLE_GARAMOND_BOLD_12));
   text_layer_set_font(s_forecast_layer, s_forecast_font);
   // alignment
@@ -106,20 +123,29 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_date_top_layer, GTextAlignmentLeft);
   text_layer_set_text_alignment(s_date_btm_layer, GTextAlignmentRight);
-  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_local_layer, GTextAlignmentRight);
   text_layer_set_text_alignment(s_forecast_layer, GTextAlignmentCenter);
 
   // Battery
   BatteryChargeState charge_state = battery_state_service_peek();
   snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%%", charge_state.charge_percent);
   text_layer_set_text(s_battery_layer, s_battery_buffer);
+  if(charge_state.charge_percent <= 20) {
+    text_layer_set_text_color(s_battery_layer, GColorRed);
+  } else if(charge_state.charge_percent <= 50) {
+    text_layer_set_text_color(s_battery_layer, GColorChromeYellow);
+  } else {
+    text_layer_set_text_color(s_battery_layer, GColorWhite);    
+  }
   // Time
   text_layer_set_text(s_time_layer, "00:00");
   // Date
   text_layer_set_text(s_date_top_layer, "Sunday");
   text_layer_set_text(s_date_btm_layer, "26 October");
   // Weather
-  text_layer_set_text(s_weather_layer, "Loading...");
+  text_layer_set_text(s_temperature_layer, "Loading...");
+  text_layer_set_text(s_local_layer, "Loading...");
   // Forecast
   text_layer_set_text(s_forecast_layer, "Loading...");
 
@@ -128,7 +154,8 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_top_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_btm_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_temperature_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_local_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_forecast_layer));
 
   // Show the correct state of the BT connection from the start
@@ -143,19 +170,23 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_top_layer);
   text_layer_destroy(s_date_btm_layer);
-  text_layer_destroy(s_weather_layer);
+  text_layer_destroy(s_temperature_layer);
+  text_layer_destroy(s_local_layer);
   text_layer_destroy(s_forecast_layer);
   // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
   gbitmap_destroy(s_bt_icon_bitmap);
+  gbitmap_destroy(s_weather_icon_bitmap);
   // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
   bitmap_layer_destroy(s_bt_icon_layer);
+  bitmap_layer_destroy(s_weather_icon_layer);
   // Unload GFont
   fonts_unload_custom_font(s_battery_font);
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_date_font);
-  fonts_unload_custom_font(s_weather_font);
+  fonts_unload_custom_font(s_temperature_font);
+  fonts_unload_custom_font(s_local_font);
   fonts_unload_custom_font(s_forecast_font);
   // Battery State Service
   battery_state_service_unsubscribe();
@@ -180,20 +211,69 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
+  static char status_buffer[16];
   static char temperature_buffer[8];
   static char conditions_buffer[32];
-  static char weather_layer_buffer[32];
+  static char icon_buffer[8];
+  static char local_buffer[32];
   // Read tuples for data
+  Tuple *status_tuple = dict_find(iterator, MESSAGE_KEY_REQSTATUS);
   Tuple *temp_tuple = dict_find(iterator, MESSAGE_KEY_TEMPERATURE);
   Tuple *conditions_tuple = dict_find(iterator, MESSAGE_KEY_CONDITIONS);
+  Tuple *icon_tuple = dict_find(iterator, MESSAGE_KEY_ICONNAME);
+  Tuple *local_tuple = dict_find(iterator, MESSAGE_KEY_LOCALNAME);
 
   // If all data is available, use it
   if(temp_tuple && conditions_tuple) {
+    snprintf(status_buffer, sizeof(status_buffer), "%s", status_tuple->value->cstring);
     snprintf(temperature_buffer, sizeof(temperature_buffer), "%d°c", (int)temp_tuple->value->int32);
     snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    snprintf(icon_buffer, sizeof(icon_buffer), "%s", icon_tuple->value->cstring);
+    snprintf(local_buffer, sizeof(local_buffer), "%s", local_tuple->value->cstring);
     // Assemble full string and display
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s  %s", temperature_buffer, conditions_buffer);
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+    text_layer_set_text(s_temperature_layer, temperature_buffer);
+    text_layer_set_text(s_local_layer, local_buffer);
+    // Weather icon
+    if(strcmp(icon_buffer, "01d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_01D);
+    } else if(strcmp(icon_buffer, "01n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_01N);
+    } else if(strcmp(icon_buffer, "02d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_02D);
+    } else if(strcmp(icon_buffer, "02n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_02N);
+    } else if(strcmp(icon_buffer, "03d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_03D);
+    } else if(strcmp(icon_buffer, "03n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_03N);
+    } else if(strcmp(icon_buffer, "04d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_04D);
+    } else if(strcmp(icon_buffer, "04n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_04N);
+    } else if(strcmp(icon_buffer, "09d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_09D);
+    } else if(strcmp(icon_buffer, "09n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_09N);
+    } else if(strcmp(icon_buffer, "10d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_10D);
+    } else if(strcmp(icon_buffer, "10n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_10N);
+    } else if(strcmp(icon_buffer, "11d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_11D);
+    } else if(strcmp(icon_buffer, "11n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_11N);
+    } else if(strcmp(icon_buffer, "13d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_13D);
+    } else if(strcmp(icon_buffer, "13n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_13N);
+    } else if(strcmp(icon_buffer, "50d") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_50D);
+    } else if(strcmp(icon_buffer, "50n") == 0) {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_50N);
+    } else {
+      s_weather_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_W_99);
+    }
+    bitmap_layer_set_bitmap(s_weather_icon_layer, s_weather_icon_bitmap);
   }
   text_layer_set_text(s_forecast_layer, "");
 }
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -11,8 +11,11 @@ function locationSuccess(pos) {
   // We will request the weather here
   // Construct URL
   var myAPIKey = 'ea89120ba85fc38f32f1eebc9063f758';
+  var lat = pos.coords.latitude;
+  var lon = pos.coords.longitude;
   var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
+      lat + '&lon=' + lon + '&appid=' + myAPIKey;
+  console.log('Url is ' + url);
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET', 
@@ -20,18 +23,43 @@ function locationSuccess(pos) {
       // responseText contains a JSON object with weather info
       var json = JSON.parse(responseText);
 
-      // Temperature in Kelvin requires adjustment
-      var temperature = Math.round(json.main.temp - 273.15);
-      console.log('Temperature is ' + temperature);
+      // Status
+      var cod = json.cod;
+      console.log('Cod is ' + cod);
+      
+      var status = '';
+      var temperature = 0;
+      var conditions = '';
+      var icon = '';
+      var name = '';
 
-      // Conditions
-      var conditions = json.weather[0].main;      
+      if (cod === 200) {
+        status = 'success';
+        // Temperature in Kelvin requires adjustment
+        temperature = Math.round(json.main.temp - 273.15);
+        // Conditions
+        conditions = json.weather[0].main;
+        icon = json.weather[0].icon;
+        name = json.name;
+      } else if (cod === '404') {
+        status = json.message;
+      } else {
+        status = 'failure';
+      }
+
+      console.log('Status is ' + status);
+      console.log('Temperature is ' + temperature);
       console.log('Conditions are ' + conditions);
+      console.log('Icon is ' + icon);
+      console.log('Name is ' + name);
 
       // Assemble dictionary using our keys
       var dictionary = {
+        'REQSTATUS': status,
         'TEMPERATURE': temperature,
-        'CONDITIONS': conditions
+        'CONDITIONS': conditions,
+        'ICONNAME': icon,
+        'LOCALNAME': name
       };
 
       // Send to Pebble
